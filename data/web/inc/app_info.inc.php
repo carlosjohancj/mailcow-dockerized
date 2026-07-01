fc5cbf85a3bb32abc1b66fccb2ff1dbfcd6ad622
Carlos Montilva <carlosjohancj@gmail.com>
2026-06-30 20:47:05 -0400

    all

---
+++ data/web/inc/app_info.inc.php
@@ -0,0 +1,11 @@
+<?php
+  $MAILCOW_GIT_VERSION="2026-05c";
+  $MAILCOW_LAST_GIT_VERSION="";
+  $MAILCOW_GIT_OWNER="mailcow";
+  $MAILCOW_GIT_REPO="mailcow-dockerized";
+  $MAILCOW_GIT_URL="https://github.com/mailcow/mailcow-dockerized";
+  $MAILCOW_GIT_COMMIT="2ac4b1deaee50e1284d644cecc16dcb0b37e67e2";
+  $MAILCOW_GIT_COMMIT_DATE="2026-05-26 10:50:58 +0200";
+  $MAILCOW_BRANCH="master";
+  $MAILCOW_UPDATEDAT=1782865285;
+?>
